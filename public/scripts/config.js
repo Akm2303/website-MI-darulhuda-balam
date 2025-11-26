5fa82c693661f2f39a43fc2f8b9823ec9af493cc
Jhonod <akmjonod@gmail.com>
2025-11-26 16:26:04 +0700

    update deui

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,9 +1,7 @@
 // Konfigurasi Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
-  import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
-  import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyApNKH811nkaAiGnP4TjOlR57VPr_Keghg",
@@ -15,14 +13,9 @@ const firebaseConfig = {
     measurementId: "G-4P4770WH6T"
   };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  // Services (sama seperti v8 tetapi modular)
-  const analytics = getAnalytics(app);
-  window.auth = getAuth(app);
-  window.db = getDatabase(app);
-  window.firestore = getFirestore(app);
-  window.storage = getStorage(app);
 
-  console.log("Firebase v12 connected successfully");
+// Initialize Cloud Storage and get a reference to the service
+const storage = getStorage(app);
