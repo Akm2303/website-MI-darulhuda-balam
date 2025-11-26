2f247b9a94156396422b8e5445fc8ee8572ca07b
Jhonod <akmjonod@gmail.com>
2025-11-26 15:51:24 +0700

    update deui

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCFlm5SqLoSpUQL0Eo9Htj_aDuBLumplT8",
     authDomain: "darul-huda-web-ae9fa.firebaseapp.com",
     projectId: "darul-huda-web-ae9fa",
-    storageBucket: "darul-huda-web-ae9fa.firebasestorage.app",
+    storageBucket: "darul-huda-web-ae9fa.firebasestorage.com",
     messagingSenderId: "343029892756",
     appId: "1:343029892756:web:1f165c145c485b5716d681",
     measurementId: "G-R7ZKC98FLF"
