b7e471a050958e5d09c2a8cd10b525751aae3ece
Jhonod <akmjonod@gmail.com>
2025-11-26 17:21:16 +0700

    update deui

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,11 +1,11 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyApNKH811nkaAiGnP4TjOlR57VPr_Keghg",
-  authDomain: "darulhuda-balam.firebaseapp.com",
-  databaseURL: "https://darulhuda-balam-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "darulhuda-balam",
-  storageBucket: "darulhuda-balam.firebasestorage.app",
-  messagingSenderId: "200089179332",
-  appId: "1:200089179332:web:c3a904f43ebe79a1b8e591",
-  measurementId: "G-4P4770WH6T"
-};
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyApNKH811nkaAiGnP4TjOlR57VPr_Keghg",
+//   authDomain: "darulhuda-balam.firebaseapp.com",
+//   databaseURL: "https://darulhuda-balam-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "darulhuda-balam",
+//   storageBucket: "darulhuda-balam.firebasestorage.app",
+//   messagingSenderId: "200089179332",
+//   appId: "1:200089179332:web:c3a904f43ebe79a1b8e591",
+//   measurementId: "G-4P4770WH6T"
+// };
