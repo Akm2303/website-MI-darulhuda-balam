27f28779a56371a6050d219a08225accbc940b64
Jhonod <akmjonod@gmail.com>
2025-11-27 10:35:48 +0700

    update

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,11 +1,14 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyApNKH811nkaAiGnP4TjOlR57VPr_Keghg",
-  authDomain: "darulhuda-balam.firebaseapp.com",
-  databaseURL: "https://darulhuda-balam-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "darulhuda-balam",
-  storageBucket: "darulhuda-balam.appspot.com",
-  messagingSenderId: "200089179332",
-  appId: "1:200089179332:web:c3a904f43ebe79a1b8e591",
-  measurementId: "G-4P4770WH6T"
-};
+ // Firebase initialization
+        const firebaseConfig = {
+            apiKey: "AIzaSyApNKH811nkaAiGnP4TjOlR57VPr_Keghg",
+            authDomain: "darulhuda-balam.firebaseapp.com",
+            projectId: "darulhuda-balam",
+            databaseURL: "https://darulhuda-balam-default-rtdb.asia-southeast1.firebasedatabase.app",
+            storageBucket: "darulhuda-balam.firebasestorage.app",
+            messagingSenderId: "1:200089179332:web:c3a904f43ebe79a1b8e591",
+            appId: "G-4P4770WH6T"
+        };
+
+        firebase.initializeApp(firebaseConfig);
+        const auth = firebase.auth();
+        const db = firebase.firestore();
